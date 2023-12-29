7c0292d60aa82105e36d1b92464dd31dd794b8b1
vyshnav <vyshnavofc@gmail.com>
2023-12-29 19:03:20 +0530

    resolved errors

--- pages/about.js
+++ pages/about.js
@@ -26,7 +26,7 @@ const about = () => {
         <p className="text-4xl font-semibold ">WHO I AM</p>
         <p className="w-16 h-1 rounded-full mt-5 bg-slate-400"></p>
         <p className="text-[18px] leading-7 text-slate-500 text-justify mt-10" >
-Jomon Joseph is a business consultant and modelling expert with an excellent track record of having delivered substantial value to a wide variety of institutions in the public and private sectors and academia.Consulting assignments primarily include business model design, learning content development and delivery as well as financial planning and advisory services (individual and corporate). Jomon began his career with a major broking house in Mumbai having served as Senior Analyst, Head of Research and Financial Planner. At present, he is the promoter of 'The Strategist'.
+Jomon Joseph is a business consultant and modelling expert with an excellent track record of having delivered substantial value to a wide variety of institutions in the public and private sectors and academia.Consulting assignments primarily include business model design, learning content development and delivery as well as financial planning and advisory services (individual and corporate). Jomon began his career with a major broking house in Mumbai having served as Senior Analyst, Head of Research and Financial Planner. At present, he is the promoter of The Strategist.
         </p>
         </div>
       </div>
